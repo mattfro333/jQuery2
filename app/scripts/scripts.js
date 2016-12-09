18b3f1d7186f0d7b4b2e513b9f338c4de4c163fd
mattfro333 <mattfro333@gmail.com>
2016-12-08 20:46:35 -0700

    JqueryPractice

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -5,13 +5,7 @@ $(document).ready(function() {
     this.task = task;
     this.id = 'new';
   }
-var addTask = function(task) {
-  if(task) {
-    task = new Task(task);
-    listo.push(task);
-  }
-};
-var addTask = function(task) {
+
   var advanceTask = function(task) {
     var modified = task.innerText.trim()
     for (var i = 0; i < listo.length; i++) {
@@ -28,39 +22,40 @@ var addTask = function(task) {
     }
     task.remove();
   };
-  if(task) {
-    task = new Task(task);
-    listo.push(task);
-    $('#newItemInput').val('');
-    $('#newList').append(
-        '<a href="#finish" class="" id="item">' +
-        '<li class="list-group-item">' +
-        '<h3>' + task.task + '</h3>' +
-        '<span class="arrow pull-right">' +
-        '<i class="glyphicon glyphicon-arrow-right">' +
-        '</span>' +
-        '</li>' +
-        '</a>'
-    );
-  }
-  $('#newTaskForm').slideToggle('fast', 'linear');
-};
-$('#saveNewItem').on('click', function (e){
-    e.preventDefault();
-    var task = $('#newItemInput').val().trim();
-    addTask(task);
-});
+
+  var addTask = function(task) {
+  	if(task) {
+  		task = new Task(task);
+  		listo.push(task);
+
+  		$('#newItemInput').val('');
+  		  $('#newList').append(
+                          '<a href="#finish" class="" id="item">' +
+                          '<li class="list-group-item">' +
+                          '<h3>' + task.task + '</h3>'+
+                          '<span class="arrow pull-right">' +
+                          '<i class="glyphicon glyphicon-arrow-right">' +
+                          '</span>' +
+                          '</li>' +
+                          '</a>'
+                      );
+
+  	}
+    $('#newTaskForm').slideToggle('fast', 'linear');
+  };
+
+
+  $('#saveNewItem').on('click', function (e) {
+      e.preventDefault();
+      var task = $('#newItemInput').val().trim();
+      addTask(task);
+  });
 $('#add-todo').on('click', function () {
     $('#newTaskForm').fadeToggle('fast', 'linear');
 });
-$(document).on('click', '#item', function(e) {
-	e.preventDefault();
-});
-$(document).on('click', '#item', function(e) {
-	e.preventDefault();
-  var task = this;
-  advanceTask(task);
-  this.id = 'inProgress';
+$('#cancel').on('click', function (e) {
+    e.preventDefault();
+    $('#newTaskForm').fadeToggle('fast', 'linear');
 });
 $(document).on('click', '#item', function(e) {
   e.preventDefault();
@@ -77,6 +72,7 @@ $(document).on('click', '#inProgress', function (e) {
   advanceTask(task);
   $('#archivedList').append(changeIcon);
 });
+
 $(document).on('click', '#archived', function (e) {
   e.preventDefault();
   var task = this;
